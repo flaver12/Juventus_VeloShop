909a42ab3a2b7a7fd177be29bd200bd05b846039
Flavio Kleiber <flaverkleiber@yahoo.de>
2023-03-21 20:10:09 +0100

    Make it more nice 👑

--- src/org/fk/vs/data/Staff.java
+++ src/org/fk/vs/data/Staff.java
@@ -1,11 +1,12 @@
 package org.fk.vs.data;
 
-public class Staff extends User{
+public class Staff extends User {
+
+   private String shop;
+
    public Staff(int id, String name) {
         super(id, name);
     }
-
-private String shop;
 
     public String getShop() {
         return shop;
